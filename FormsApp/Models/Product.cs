f5657523cd4f3ec2dfe8a422aa79335b6e5b7580
musta <musta@MUSTAFA>
2024-07-10 15:00:58 +0300

    Add project files.

---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+    public class Product
+    {
+
+        [Display(Name = "Id")]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Display(Name = "Price")]
+        [Required]
+        [Range(0, 999999)]
+        public decimal? Price { get; set; }
+
+        [Display(Name = "Image")]
+        //[StringLength(9999, ErrorMessage ="{0} length must be between {2} and {1}",MinimumLength =1)]
+        public string Image { get; set; } = string.Empty ;
+        public bool IsActive { get; set; }
+
+
+        [Display(Name="Category")]
+        [Required]
+        public int? CategoryId { get; set; }
+    }
+}
---
+++ FormsApp/Models/Repository.cs
@@ -0,0 +1,75 @@
+﻿namespace FormsApp.Models
+{
+    public class Repository
+    {
+        private static readonly List<Product> _products = new();
+        private static readonly List<Category> _categories = new();
+        static Repository()
+        {
+            _categories.Add(new Category { CategoryId = 1, Name = "Phone" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Computer" });
+
+            _products.Add(new Product { ProductId = 1, Name="IPhone 14" , Price=40000, IsActive=true, Image="14.jpg", CategoryId=1 });
+            _products.Add(new Product { ProductId = 2, Name="IPhone 14 Pro" , Price=50000, IsActive=true, Image="14pro.jpg", CategoryId=1 });
+            _products.Add(new Product { ProductId = 3, Name="IPhone 15" , Price=52000, IsActive=false, Image="15.jpg", CategoryId=1 });
+            _products.Add(new Product { ProductId = 4, Name="IPhone 15 Pro Max" , Price=60000, IsActive=false, Image="15promax.jpg", CategoryId=1 });
+            _products.Add(new Product { ProductId = 5, Name="Macbook Pro" , Price=90000, IsActive=true, Image="pro.jpg", CategoryId=2 });
+            _products.Add(new Product { ProductId = 6, Name="Macbook Air" , Price=80000, IsActive=true, Image="air.jpg", CategoryId=2 });
+        }
+
+        public static List<Product> Products
+        {
+
+            get
+            {
+                return _products;
+            }
+        }
+
+        public static void CreateProduct(Product entity)
+        {
+            _products.Add(entity);
+        }
+
+        public static void EditProduct(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p=>p.ProductId == updatedProduct.ProductId);
+            if (entity != null) {
+                if (!string.IsNullOrEmpty(updatedProduct.Name))
+                {
+                    entity.Name = updatedProduct.Name;
+                    entity.Price = updatedProduct.Price;
+                    entity.Image = updatedProduct.Image;
+                    entity.CategoryId = updatedProduct.CategoryId;
+                    entity.IsActive = updatedProduct.IsActive;
+                }
+            }
+        }
+
+        public static void EditIsActive(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
+            if (entity != null)
+            {
+                
+                    entity.IsActive = updatedProduct.IsActive;
+                
+            }
+        }
+        public static void DeleteProduct(Product deletedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
+            if (entity != null)
+            {
+                _products.Remove(entity);
+            }
+        }
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+        }
+    }
+}
